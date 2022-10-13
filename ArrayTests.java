b8f56e7ba7720b09236f0ce69c94866746fa3960
kondratyusha <94666967+kondratyusha@users.noreply.github.com>
2022-10-13 16:37:23 -0700

    Fixed ArrayExamples and ArrayTests

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,15 +4,15 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+    int[] input1 = {1,3,5};
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{5,3,1}, input1);
 	}
 
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {0,1,2,3,4,5,6,7,8,9};
+    assertArrayEquals(new int[]{9,8,7,6,5,4,3,2,1,0}, ArrayExamples.reversed(input1));
   }
 }
