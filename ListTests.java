cc378b259e25769295640ca335ca643b43968e39
kondratyusha <94666967+kondratyusha@users.noreply.github.com>
2022-10-13 18:22:30 -0700

    ListTests

--- ListTests.java
+++ ListTests.java
@@ -36,4 +36,26 @@ public class ListTests {
 
 
     }
+
+    @Test
+    public void testMerge(){
+        List<String> input1 = new ArrayList<>();
+        List<String> input2 = new ArrayList<>();
+        List<String> both = new ArrayList<>();
+
+        input1.add("apple");
+        input1.add("banana");
+        input1.add("pineapple");
+
+        input2.add("orange");
+        input2.add("pomegranate");
+
+        both.addAll(input1);
+        both.addAll(input2);
+        both.sort(null);
+
+        assertEquals(both, ListExamples.merge(input1, input2));
+
+
+    }
 }
